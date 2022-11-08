cd29808e02b2cd0bcc9ad1cc556c3c504e5ee008
AlexShim99 <shimchungsiong@graduate.utm.my>
2022-11-07 22:43:49 -0800

    3rd commit

---
+++ 8thNov.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+first_int = int(input("Insert first integer: "))
+#first_int = 10
+second_int = 20
+if first_int > second_int:
+	print("The first int is bigger!")
+else:
+	print("The second int is bigger!")
